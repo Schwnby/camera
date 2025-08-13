8d3a975db927108dffbb68f3f07c9c3f5dfe3ea6
Simon.Unger <Simon.Unger@digitecgalaxus.ch>
2025-08-13 08:42:00 +0200

    created views and urls

---
+++ Camera/views.py
@@ -0,0 +1,20 @@
+﻿from django.http import HttpResponse
+from picamera2 import Picamera2
+from PIL import Image
+import io
+
+def index(request):
+    picam2 = Picamera2()
+
+    picam2.start()
+
+    frame = picam2.capture_array()
+
+    picam2.close()
+
+    image = Image.fromarray(frame)
+    buffer = io.BytesIO()
+    image.save(buffer, format='PNG')
+    buffer.seek(0)
+
+    return HttpResponse(buffer, content_type='image/png')
