448daf2639be070b56fb6c05cc5a06cb9e48864e
Simon Unger <simon.unger@digitecgalaxus.ch>
2025-09-02 18:10:13 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -58,7 +58,6 @@ def configure():
     
 configure()
 application = get_wsgi_application()
-app = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
