47d41c800c3f8ecb5af4ba25c57d2504dac2b223
Simon Unger <simon.unger@digitecgalaxus.ch>
2025-09-08 17:11:21 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 
@@ -20,34 +21,36 @@ if not settings.configured:
             'django.middleware.csrf.CsrfViewMiddleware',
             'django.middleware.clickjacking.XFrameOptionsMiddleware',
         ),
-    )  
+    )
 
-format = 'BMP'
-content_type = 'image/bmp'
+format = 'JPG'
+content_type = 'image/jpg'
 
 from django.http import HttpResponse
 from picamera2 import Picamera2
 from PIL import Image
 import io
 import cv2
-import time
 import json
 
 def take_picture():
     picam2 = Picamera2()
+
+    picam2.set_controls({"ExposureTime": 500})
     config = picam2.create_still_configuration()
     picam2.configure(config)
 
     picam2.start()
-    time.sleep(2)
 
     frame = picam2.capture_array()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('test.png', frame)
 
     picam2.stop()
     picam2.close()
-    
+
     return frame
-    
+
 def index(request):
     frame = take_picture()
 
@@ -55,7 +58,7 @@ def index(request):
     buffer = io.BytesIO()
     image.save(buffer, format=format)
     buffer.seek(0)
-    
+
     return HttpResponse(buffer.getvalue(), content_type=content_type)
 
 
@@ -67,29 +70,28 @@ urlpatterns = [
 
 
 from django.core.wsgi import get_wsgi_application
-    
+
 def configure():
     image = take_picture()
-    
-    image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
+
     detector = cv2.QRCodeDetector()
-    data, bbox, straight_qrcode = detector.detectAndDecode(image_bgr)
-    
-    if bbox is not None:        
+    data, bbox, straight_qrcode = detector.detectAndDecode(image)
+
+    if bbox is not None:
+        global format
+        global content_type
+        print(f"data: {data}")
         parsed = json.loads(data)
         format = parsed["format"]
         content_type = parsed["content_type"]
     else:
         print("No QR code found")
-    
-    time.sleep(5)
-    
+
 configure()
 application = get_wsgi_application()
 
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
-    
+
     execute_from_command_line(sys.argv)
